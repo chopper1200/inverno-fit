70f8ac56f035f8f0ec3c910dfea524f715ea9a85
chopper1200 <matb4@hotmail.it>
2026-09-22 15:50:21 +0200

    Migliora la scheda tapis roulant

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='inverno-v5-2';
+const CACHE='inverno-v5-3';
 const FILES=['./','./index.html','./style.css','./data.js','./app.js','./manifest.webmanifest','./icon.svg','./apple-touch-icon.png','./piano-originale.pdf',...['006','012','018','024','030'].map(n=>'./images/piano-'+n+'.jpg')];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('message',event=>{if(event.data==='ACTIVATE_UPDATE')self.skipWaiting();});

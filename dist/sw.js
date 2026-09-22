d93325612b51c19651e099b061e38558b002b19e
chopper1200 <matb4@hotmail.it>
2026-09-22 08:03:57 +0200

    Delete dist directory

--- dist/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='inverno-v2-1';
-const FILES=['./','./index.html','./style.css','./data.js','./app.js','./manifest.webmanifest','./icon.svg','./piano-originale.pdf',...['006','012','018','024','030'].map(n=>'./images/piano-'+n+'.jpg')];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('inverno-')&&k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',event=>{const url=new URL(event.request.url);if(event.request.method!=='GET'||url.origin!==self.location.origin)return;const allowed=FILES.some(p=>new URL(p,self.registration.scope).href===url.href);if(!allowed)return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));});
